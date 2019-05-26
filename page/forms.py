4aa212d0836cd67132297cfecde4b0d52618d151
Israel Ebonko <ebonko2017ip@gmail.com>
2019-05-26 15:35:02 +0100

    improved some syntax

--- page/forms.py
+++ page/forms.py
@@ -2,10 +2,5 @@
 from django import forms
 from .models import Bvn
 
-'''class BvnForm(forms.ModelForm):
-    class Meta:
-        model = Bvn
-        fields = ['bvnumber']'''
-
 class BvnForm(forms.Form):
 	bvnumber = forms.CharField(max_length=20)
--- page/views.py
+++ page/views.py
@@ -3,54 +3,6 @@ from . import forms
 from .models import Bvn
 import requests
 
-# Create your views here.
-'''def index(request):
-    url = 'https://ravesandboxapi.flutterwave.com/v2/kyc/bvn/{}?seckey=FLWSECK-4d4e435aa173a758ea07fc9e48e824b7-X'
-    bvn = 12345678901
-    r = requests.get(url.format(bvn)).json()
-
-    context = {
-        "bvn": bvn,
-        "first_name": r['data']['first_name'],
-        "last_name":r['data']['last_name'],
-        "date_of_birth":r['data']['date_of_birth'],
-        "phone_number":r['data']['phone_number'],
-    }
-    print(context)
-    return render(request, 'page/index.html', context)'''
-
-'''class index(CreateView):
-    template_name= 'bvn/bvn_create.html'
-    form_class = forms.BvnForm
-    print(form_class.cleaned_data)
-    queryset = Bvn.objects.all()'''
-
-'''def index(request):
-    data_values = {}
-    form = forms.BvnForm()
-    if request.method == "POST":
-        form = forms.BvnForm(request.POST)
-        if form.is_valid():
-            value = form.cleaned_data
-            url = 'https://ravesandboxapi.flutterwave.com/v2/kyc/bvn/{}?seckey=FLWSECK-4d4e435aa173a758ea07fc9e48e824b7-X'
-            bvn = value['bvnumber']
-            r = requests.get(url.format(bvn)).json()
-            data_values = {
-                "bvn": bvn,
-                "first_name": r['data']['first_name'],
-                "last_name":r['data']['last_name'],
-                "date_of_birth":r['data']['date_of_birth'],
-                "phone_number":r['data']['phone_number'],
-            }
-            return redirect('suc/')
-            print(data_values)
-            Bvn.objects.create(**form.cleaned_data)
-        else: 
-            print(form.errors)
-    context = { 'form': form,
-        'data_values' :data_values}
-    return render(request, 'page/index.html', context)'''
-
 def index(request):
     data_values = {}
     form = forms.BvnForm()
@@ -58,7 +10,7 @@ def index(request):
         form = forms.BvnForm(request.POST)
         if form.is_valid():
             Bvn.objects.create(**form.cleaned_data)
-            redirect('verification/successpage/')
+            return redirect('verification/success/')
         else:
             print(form.errors)
     context = { 'form': form}
@@ -77,6 +29,7 @@ def successpage(request):
         "date_of_birth":r['data']['date_of_birth'],
         "phone_number":r['data']['phone_number'],
     }
+    print(data_values['first_name'])
     context = {
         'data_values' : data_values
     }
